4973e7ab14fa8e01984a4dc61dd69c7c4f5f5cbc
anton <eagoker@mail.ru>
2024-10-09 22:02:41 -0400

    increment 2. Small fixes

--- internal/handlers/convertToShort_test.go
+++ internal/handlers/convertToShort_test.go
@@ -8,33 +8,28 @@ import (
 )
 
 func TestConvertToShort(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
 	tests := []struct {
 		name               string
 		requestBody       []byte
 		expectedStatus    int
-		expectedBody      string
+		expectedBodyPrefix string
 	}{
 		{
-			name:            "Successful conversion",
-			requestBody:    []byte("https://example.com/original/url"),
-			expectedStatus: http.StatusCreated,
-			expectedBody:   "", // Тело ответа будет рандомным, поэтому можно оставить пустым или использовать регулярное выражение
+			name:               "Successful conversion",
+			requestBody:       []byte("https://example.com/original/url"),
+			expectedStatus:    http.StatusCreated,
+			expectedBodyPrefix: "http://localhost:8080/", // Убедитесь, что это корректный префикс
 		},
 		{
 			name:            "Method not allowed",
 			requestBody:    nil,
 			expectedStatus: http.StatusMethodNotAllowed,
-			expectedBody:   "",
+			expectedBodyPrefix: "", // Нет тела для метода, не поддерживаемого
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Создаем тестовый запрос
 			var req *http.Request
 			if tt.requestBody != nil {
 				req = httptest.NewRequest(http.MethodPost, "/convert", bytes.NewBuffer(tt.requestBody))
@@ -45,20 +40,13 @@ func TestConvertToShort(t *testing.T) {
 
 			ConvertToShort(recorder, req)
 
-			// Проверяем код состояния
 			if status := recorder.Code; status != tt.expectedStatus {
 				t.Errorf("handler returned wrong status code: got %v want %v",
 					status, tt.expectedStatus)
 			}
 
-			// Если это успешный случай, то проверяем, что тело ответа не пустое
-			if tt.expectedStatus == http.StatusCreated && recorder.Body.String() == "" {
-				t.Errorf("handler returned empty body for successful conversion")
-			}
-
-			// В случае, если хотите проверить, что тело ответа соответствует формату короткого URL
 			if tt.expectedStatus == http.StatusCreated {
-				if !bytes.HasPrefix(recorder.Body.Bytes(), []byte("http://localhost:8080/")) {
+				if !bytes.HasPrefix(recorder.Body.Bytes(), []byte(tt.expectedBodyPrefix)) {
 					t.Errorf("handler returned unexpected body: got %v", recorder.Body.String())
 				}
 			}
